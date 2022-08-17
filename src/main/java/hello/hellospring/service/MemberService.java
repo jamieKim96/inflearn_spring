ac8478df3211ea7302287eb489fdd1bd1c81121c
JamieKim <jjyju1344@gmail.com>
2022-08-17 11:38:22 +0900

    refactor: MemberService 주석 정리

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -9,7 +9,7 @@ import java.util.Optional;
 
 public class MemberService {
 
-    //1 회원 리포지토리 객체 생성
+    //1
     private final MemberRepository memberRepository;
 
     public MemberService(MemberRepository memberRepository) {  //memberRepository를 직접 생성하는 것이 아니라 외부에서 넣어주도록 함
@@ -28,15 +28,14 @@ public class MemberService {
         result.ifPresent(m -> {
             throw new IllegalStateException("이미 존재하는 값입니다")
         });*/
-        //6 Optional 메소드인 memberRepository 바로 뒤에 .ifPresent 메소드 넣기 가능
-        validateDuplicateMember(member); //7 중복회원 검증 메소드 Extract (ctrl + alt + shift + t)
-        memberRepository.save(member);   //2 member 정보 저장
-        return member.getId();           //3 저장 후 memberID 반환
+        validateDuplicateMember(member); //7
+        memberRepository.save(member);   //2
+        return member.getId();           //3
     }
 
     private void validateDuplicateMember(Member member) {
-        memberRepository.findByName(member.getName())  //4 name으로 member 조회
-                .ifPresent(m -> {           //5 ifPresent() : null이 아닌 어떤 값이 있으면 내부 로직 동작(Optional이라 가능한 메소드)
+        memberRepository.findByName(member.getName())  //4
+                .ifPresent(m -> {           //5
                     throw new IllegalStateException("이미 존재하는 회원입니다");
                 });
     }
